b765ff563e3f70c87cccb143cd719291ca177222
Shamil <66209982+shamilsan@users.noreply.github.com>
2022-10-13 19:26:48 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const exec = require('child_process');
+
+exec('pkill -f \'gear |gear$\' -9', (err, stdout, stderr) => {
+  if (err) {
+    console.log(`JS_TEST: Unable to execute kill command (${err})`);
+  }
+
+  if (exitCode == 0) {
+    console.log('JS_TEST: ✅ Test passed');
+  } else {
+    console.log(`JS_TEST: ❌ Test failed (${exitCode})`);
+  }
+  process.exit(exitCode);
+}
